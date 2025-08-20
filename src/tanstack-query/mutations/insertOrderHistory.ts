9df73d7caef3e65ee4740e37af74ca13ea591aeb
Nathan Lopez <nathancarllopez@gmail.com>
2025-08-20 16:47:55 -0500

    These two mutations are used in the ReportDisplay.tsx component to finalize the changes made during order processing

---
+++ src/tanstack-query/mutations/insertOrderHistory.ts
@@ -0,0 +1,32 @@
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "../QueryClientProvider";
+import type { InsertOrderHistoryRow, OrderHistoryRow } from "@/types/rpmp-types";
+import { supabase } from "@/supabase/client";
+import { snakeToCamel } from "../key-converters";
+
+export function useInsertOrderHistoryMutation() {
+  return useMutation({
+    mutationFn: insertOrderHistory,
+    onSuccess: (data) =>
+      queryClient.setQueryData(["orderHistory"], (curr: OrderHistoryRow[] = []) => [data, ...curr]),
+  });
+}
+
+async function insertOrderHistory(
+  newOrder: InsertOrderHistoryRow
+): Promise<OrderHistoryRow> {
+  const { data, error } = await supabase
+    .from("order_history")
+    .insert(newOrder)
+    .select()
+    .single();
+
+  if (error) {
+    console.warn("Failed to insert new order");
+    console.warn(error.message);
+
+    throw error;
+  }
+
+  return snakeToCamel<OrderHistoryRow>(data);
+}
---
+++ src/tanstack-query/mutations/markBackstockUnavailable.ts
@@ -0,0 +1,36 @@
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "../QueryClientProvider";
+import { supabase } from "@/supabase/client";
+
+export function useMarkBackstockUnavailableMutation() {
+  return useMutation({
+    mutationFn: markBackstockUnavailable,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["backstock"] })
+  });
+}
+
+async function markBackstockUnavailable(usedBackstockIds: Set<number>) {
+  const { error: proteinError } = await supabase
+    .from("backstock_proteins")
+    .update({ claimed: true })
+    .in("id", Array.from(usedBackstockIds));
+
+  if (proteinError) {
+    console.warn("Failed to mark protein backstock rows as unavailable");
+    console.warn(proteinError.message);
+
+    throw proteinError;
+  }
+
+  const { error: veggieCarbError } = await supabase
+    .from("backstock_veggie_carb")
+    .update({ claimed: true })
+    .in("id", Array.from(usedBackstockIds));
+
+  if (veggieCarbError) {
+    console.warn("Failed to mark veggie carb backstock rows as unavailable");
+    console.warn(veggieCarbError.message);
+
+    throw veggieCarbError;
+  }
+}
