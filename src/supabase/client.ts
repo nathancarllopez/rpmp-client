88c8ede2cbb4a9e62e706ea8681b22783105412d
Nathan Lopez <nathancarllopez@gmail.com>
2025-08-20 14:44:25 -0500

    Added supabase and types folders. The rpmp-types file has everything commented out for now, and I'll uncomment types as I copy and paste the finished content and use each type.

---
+++ src/supabase/client.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+import type { Database } from "../types/supa-types";
+
+const PROJECT_URL = import.meta.env.VITE_SUPABASE_URL;
+const ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient<Database>(PROJECT_URL, ANON_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+  },
+});
---
+++ src/supabase/resetPassword.ts
@@ -0,0 +1,17 @@
+import { supabase } from "./client";
+
+const endpoint = "/changePassword";
+
+export default async function resetPassword(email: string): Promise<void> {
+  const redirectTo = import.meta.env.VITE_SUPABSE_REDIRECT_URL + endpoint;
+  const { error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo,
+  });
+
+  if (error) {
+    console.log("Error requesting password reset:", error.message);
+    console.log(error.code);
+
+    throw error;
+  }
+}
