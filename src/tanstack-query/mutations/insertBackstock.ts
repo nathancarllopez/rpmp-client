07e3a00570c9558e757fc25b874afd6d79c4bca4
Nathan Lopez <nathancarllopez@gmail.com>
2025-08-21 10:39:08 -0500

    Used by AddNewModal to add new backstock rows to supabase

---
+++ src/tanstack-query/mutations/insertBackstock.ts
@@ -0,0 +1,24 @@
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "../QueryClientProvider";
+import type { InsertBackstockRow } from "@/types/rpmp-types";
+import { supabase } from "@/supabase/client";
+
+export function useInsertBackstockMutation() {
+  return useMutation({
+    mutationFn: insertBackstock,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["backstock"] }),
+  });
+}
+
+async function insertBackstock(newBackstock: InsertBackstockRow[]) {
+  const { error } = await supabase
+    .from("backstock_proteins")
+    .insert(newBackstock);
+
+  if (error) {
+    console.warn("Failed to insert new backstock rows");
+    console.warn(error.message);
+
+    throw error;
+  }
+}
