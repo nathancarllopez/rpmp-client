44849c63fe9be3a22c860efa2490abbf6a7662ea
Nathan Lopez <nathancarllopez@gmail.com>
2025-08-21 10:39:31 -0500

    Used by EditSelectedModal to update and/or delete backstock rows

---
+++ src/tanstack-query/mutations/updateBackstock.ts
@@ -0,0 +1,58 @@
+import { useMutation } from "@tanstack/react-query";
+import type { PostgrestError } from "@supabase/supabase-js";
+import { queryClient } from "../QueryClientProvider";
+import type { UpdateBackstockInfo } from "@/types/rpmp-types";
+import { supabase } from "@/supabase/client";
+
+export function useUpdateBackstockMutation() {
+  return useMutation({
+    mutationFn: updateBackstock,
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["backstock"] }),
+  });
+}
+
+type RPCReturnType = {
+  claimed: boolean
+  created_at: string
+  deleted_on: string
+  id: number
+  weight: number
+}[] | null;
+
+async function updateBackstock(
+  updateInfo: UpdateBackstockInfo
+): Promise<UpdateBackstockInfo> {
+  const { data, error }: { data: RPCReturnType, error: PostgrestError | null } = await supabase.rpc(
+    "update_backstock_rows",
+    {
+      updates: updateInfo,
+    }
+  );
+
+  if (error) {
+    console.log(error);
+    console.warn("Error updating backstock rows");
+    console.warn(error.message);
+
+    throw error;
+  }
+
+  if (data === null) {
+    throw new Error(`No data returned after updating backstock`)
+  }
+
+  const undoUpdateInfo = data.reduce((undoInfo, row) => {
+    const idString = row.id.toString();
+
+    undoInfo[idString] = {
+      weight: row.weight,
+      created_at: row.created_at,
+      deleted_on: row.deleted_on,
+      claimed: row.claimed,
+    };
+
+    return undoInfo;
+  }, {} as UpdateBackstockInfo);
+
+  return undoUpdateInfo;
+}
