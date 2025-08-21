50445842daf50e0dd4cfddcf19fd0abc1c1e73e3
Nathan Lopez <nathancarllopez@gmail.com>
2025-08-21 10:58:43 -0500

    Called by TimecardsDisplay once the user has reviewed the finished timecards

---
+++ src/tanstack-query/mutations/insertTimecardHistory.ts
@@ -0,0 +1,35 @@
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "../QueryClientProvider";
+import type { InsertTimecardHistoryRow, TimecardHistoryRow } from "@/types/rpmp-types";
+import { supabase } from "@/supabase/client";
+import { snakeToCamel } from "../key-converters";
+
+export function useInsertTimecardHistoryMutation() {
+  return useMutation({
+    mutationFn: insertTimecardHisotry,
+    onSuccess: (data) =>
+      queryClient.setQueryData(
+        ["timecardHistory"],
+        (curr: TimecardHistoryRow[] = []) => [data, ...curr]
+      ),
+  });
+}
+
+async function insertTimecardHisotry(
+  newTimecards: InsertTimecardHistoryRow
+): Promise<TimecardHistoryRow> {
+  const { data, error } = await supabase
+    .from("timecards_history")
+    .insert(newTimecards)
+    .select()
+    .single();
+
+  if (error) {
+    console.warn("Failed to insert new timecard");
+    console.warn(error.message);
+
+    throw error;
+  }
+
+  return snakeToCamel<TimecardHistoryRow>(data);
+}
