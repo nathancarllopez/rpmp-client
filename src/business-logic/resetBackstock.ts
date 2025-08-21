f701acfc2541d1eca7c0c5c21282eede3fccec1f
Nathan Lopez <nathancarllopez@gmail.com>
2025-08-21 10:36:42 -0500

    Used during development to reset backstock changes

---
+++ src/business-logic/resetBackstock.ts
@@ -0,0 +1,19 @@
+/**
+ * Just used for testing, should be deleted or not included in production
+ */
+
+import { supabase } from "@/supabase/client";
+import { queryClient } from "@/tanstack-query/QueryClientProvider";
+
+export async function resetBackstock() {
+  const { error } = await supabase
+    .from("backstock_proteins")
+    .update({ claimed: false, deleted_on: null })
+    .gt('id', 0);
+
+  queryClient.invalidateQueries({ queryKey: ["backstock"] }),
+
+  console.log(error);
+  
+  console.log("done");
+}
