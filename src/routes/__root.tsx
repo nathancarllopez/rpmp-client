4bcec910291f4dc83398461e9471824126afd9dc
Nathan Lopez <nathancarllopez@gmail.com>
2025-08-23 16:35:47 -0500

    Added the error component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import { createRootRouteWithContext, ErrorComponent, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -16,4 +16,5 @@ export const Route = createRootRouteWithContext<RouterContext>()({
       <ReactQueryDevtools />
     </>
   ),
+  errorComponent: ErrorComponent
 });
