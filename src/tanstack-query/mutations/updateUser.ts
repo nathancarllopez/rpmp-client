cca59ed63bc611a43f4ff69400179c2dbf407c7d
Nathan Lopez <nathancarllopez@gmail.com>
2025-08-23 16:38:00 -0500

    Used by ViewEditProfile to update user info. Still needs to be tested!

---
+++ src/tanstack-query/mutations/updateUser.ts
@@ -0,0 +1,73 @@
+import { supabase } from "@/supabase/client";
+import type { ProfileRow, UpdateProfileInfo } from "@/types/rpmp-types";
+import { camelToSnake, snakeToCamel } from "../key-converters";
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "../queryClient";
+import type { SupaUpdateProfileRow } from "@/types/supa-types";
+
+export function useUpdateUserMutation() {
+  return useMutation({
+    mutationFn: updateUser,
+    onSuccess: (updatedProfile: ProfileRow) =>
+      queryClient.setQueryData(["allProfiles"], (prevData: ProfileRow[]) =>
+        prevData.map((profile) => {
+          if (profile.userId === updatedProfile.userId) {
+            return updatedProfile;
+          }
+          return profile;
+        }),
+      ),
+  });
+}
+
+async function updateUser(updateInfo: UpdateProfileInfo): Promise<ProfileRow> {
+  const { profileUpdates, newPassword, newEmail } = updateInfo;
+  const { userId } = profileUpdates;
+
+  console.log("updateInfo", updateInfo);
+
+  if (newPassword !== null || newEmail !== null) {
+    await changeEmailAndPassword(newPassword, newEmail);
+  }
+
+  const supaUpdates = camelToSnake<SupaUpdateProfileRow>(profileUpdates);
+
+  const { data, error } = await supabase
+    .from("profiles")
+    .update(supaUpdates)
+    .eq("user_id", userId)
+    .select()
+    .single();
+
+  if (error) {
+    console.warn(error.message);
+
+    throw error;
+  }
+
+  return snakeToCamel<ProfileRow>(data);
+}
+
+const endpoint = "/auth/update-user";
+
+async function changeEmailAndPassword(
+  newPassword: string | null,
+  newEmail: string | null,
+) {
+  console.log(newPassword);
+  console.log(newEmail);
+
+  const apiUrl = import.meta.env.VITE_BACKEND_URL + endpoint;
+  const response = await fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ newPassword, newEmail }),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message);
+  }
+}
