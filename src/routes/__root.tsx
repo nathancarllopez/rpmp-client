fa25e394d7f5497c8d16c4286cb855e1ca6d1ca3
Nathan Lopez <nathancarllopez@gmail.com>
2025-08-22 19:13:03 -0500

    Added ReactQueryDevTools component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import type { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 interface RouterContext {
   userId: string | null;
@@ -12,6 +13,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
     <>
       <Outlet />
       <TanStackRouterDevtools />
+      <ReactQueryDevtools />
     </>
   ),
 });
