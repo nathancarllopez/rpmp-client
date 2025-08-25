fcfbe8a38e905a4ba85cf640244d49697ede7608
Nathan Lopez <nathancarllopez@gmail.com>
2025-08-25 12:24:09 -0500

    Formatted by prettier

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,8 @@
-import { createRootRouteWithContext, ErrorComponent, Outlet } from "@tanstack/react-router";
+import {
+  createRootRouteWithContext,
+  ErrorComponent,
+  Outlet,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import type { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -16,5 +20,5 @@ export const Route = createRootRouteWithContext<RouterContext>()({
       <ReactQueryDevtools />
     </>
   ),
-  errorComponent: ErrorComponent
+  errorComponent: ErrorComponent,
 });
